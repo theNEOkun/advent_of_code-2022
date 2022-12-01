3cbd4532ce089113fab4a3e4156cb7b11bd33804
theNEOkun <sven@raswill.se>
2022-12-01 22:00:59 +0100

    mend

---
+++ src/utils/utils.cpp
@@ -0,0 +1,19 @@
+#include "utils.hpp"
+
+/**
+ * Read a file line by line into a vector and then return
+ * @param fileName is the fileName to read
+ * @return std::vector<std::string> containing all lines of the file
+ */
+std::vector<std::string> readFile(const char* fileName) {
+  std::ifstream myfile(fileName);
+  std::vector<std::string> retvec;
+
+  if (myfile.is_open()) {
+    std::string myline;
+    while(std::getline(myfile, myline)) {
+      retvec.push_back(myline);
+    }
+  }
+  return retvec;
+}

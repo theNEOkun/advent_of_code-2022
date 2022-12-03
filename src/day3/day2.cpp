356b0b51149ceae095ce424228621ef6858cef61
theNEOkun <sven@raswill.se>
2022-12-03 12:51:38 +0100

    Working on part1

--- src/day3/day2.cpp
+++
@@ -1,5 +0,0 @@
-#include "./day3.hpp"
-
-void day3::run() {
-  
-}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "day1/day1.hpp"
 #include "day2/day2.hpp"
+#include "day3/day3.hpp"
 
 void line();
 
@@ -10,6 +11,8 @@ auto main(int argc, char *argv[]) -> int
   day1::run();
   line();
   day2::run();
+  line();
+  day3::run();
   line();
 }
 

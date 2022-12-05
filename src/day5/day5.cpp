fb1632ba00fc3c47a67c77867dd26aa9568fafc5
theNEOkun <sven@raswill.se>
2022-12-05 16:43:22 +0100

    Fixed some bugs?

--- src/day5/day5.cpp
+++ src/day5/day5.cpp
@@ -2,20 +2,22 @@
 
 namespace day5 {
 
-void part1(std::vector<std::vector<std::string>> &crates, std::vector<std::vector<int>> &directions);
+void part1(std::vector<std::stack<std::string>> crates,
+           std::vector<std::vector<int>> directions);
 
-void part2(std::vector<std::vector<std::string>> &crates, std::vector<std::vector<int>> &directions);
+void part2(std::vector<std::stack<std::string>> crates,
+           std::vector<std::vector<int>> directions);
 
-
-std::vector<std::vector<std::string>>
+std::vector<std::stack<std::string>>
 fix_crates(std::vector<std::string> &crate) {
-  std::vector<std::vector<std::string>> all_crates;
+  std::vector<std::stack<std::string>> all_crates;
   for (size_t i = 0; i < crate[0].size(); i += 4) {
-    std::vector<std::string> inner_vec;
+    std::stack<std::string> inner_vec;
     for (size_t ii = 0; ii < crate.size(); ii++) {
       std::string str{crate[ii][i], crate[ii][i + 1], crate[ii][i + 2]};
-      if(str == "   ") continue; // remove whitespace
-      inner_vec.push_back(str);
+      if (str == "   ")
+        continue; // remove whitespace
+      inner_vec.push(str);
     }
     all_crates.push_back(inner_vec);
   }
@@ -31,19 +33,18 @@ fix_crates(std::vector<std::string> &crate) {
   return all_crates;
 }
 
-std::vector<std::vector<int>> 
-fix_directions(std::vector<std::string> &file) {
+std::vector<std::vector<int>> fix_directions(std::vector<std::string> &file) {
   std::vector<std::vector<int>> all_directions;
 
-  for(auto each: file) {
+  for (auto each : file) {
     std::stringstream ss(each);
     std::string temp;
     std::vector<int> inner_vec;
-    while(std::getline(ss, temp, ' ')) {
+    while (std::getline(ss, temp, ' ')) {
       try {
         auto in = std::stoi(temp);
         inner_vec.push_back(in);
-      } catch(std::invalid_argument _) {
+      } catch (std::invalid_argument e) {
         continue;
       }
     }
@@ -69,13 +70,32 @@ void run() {
   std::vector<std::string> file(&incoming[10], &incoming[incoming.size()]);
 
   auto all_crates = fix_crates(crate);
-  auto good_files = fix_directions(file);
+  auto all_directions = fix_directions(file);
 
-  day5::part1(all_crates, good_files);
-  day5::part2(all_crates, good_files);
+  day5::part1(all_crates, all_directions);
+  day5::part2(all_crates, all_directions);
 }
 
-void part1(std::vector<std::vector<std::string>> &crates, std::vector<std::vector<int>> &directions) { return; }
+void part1(std::vector<std::stack<std::string>> crates,
+           std::vector<std::vector<int>> directions) {
+  for(auto each: directions) {
+    for(int i = 0; i < each[0]; i++) {
+      auto crate = crates[each[1] - 1].top();
+      // std::printf("%d @ %d to %d %s\n", each[0], each[1], each[2], crate.c_str());
+      crates[each[1] - 1].pop();
+      crates[each[2] - 1].push(crate);
+    }
+  }
+  for(auto each: crates) {
+    for(auto str: each) {
+      std::printf("%s\t", str.c_str());
+    }
+    std::printf("\n");
+  }
+}
 
-void part2(std::vector<std::vector<std::string>> &crates, std::vector<std::vector<int>> &directions) { return; }
+void part2(std::vector<std::stack<std::string>> crates,
+           std::vector<std::vector<int>> directions) {
+  return;
+}
 } // namespace day5
--- src/day5/day5.hpp
+++ src/day5/day5.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 #include <algorithm>
 #include <sstream>
 

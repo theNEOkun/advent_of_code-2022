cd61363fb9d0cd0419a05765800090a8b6926c48
theNEOkun <sven@raswill.se>
2022-12-06 11:13:47 +0100

    Done with day5

--- src/day5/day5.cpp
+++ src/day5/day5.cpp
@@ -81,25 +81,41 @@ void run() {
 
 void part1(std::vector<std::vector<std::string>> crates,
            std::vector<std::vector<int>> directions) {
-  for(auto each: directions) {
-    // std::printf("move %d @ %d to %d\n", each[0] - 1, each[1] - 1, each[2] - 1);
-    for(int i = 0; i < each[0]; i++) {
+  std::printf("PART1\n");
+  for (auto each : directions) {
+    // std::printf("move %d @ %d to %d\n", each[0] - 1, each[1] - 1, each[2] -
+    // 1);
+    for (int i = 0; i < each[0]; i++) {
       auto crate = crates[each[1] - 1].front();
       // std::printf("move %s\n", crate.c_str());
       crates[each[1] - 1].erase(crates[each[1] - 1].begin());
       crates[each[2] - 1].insert(crates[each[2] - 1].begin(), crate);
     }
   }
-  for(auto each: crates) {
-    for(auto str: each) {
-      std::printf("%s\t", str.c_str());
-    }
-    std::printf("\n");
+  for (auto each : crates) {
+    std::printf("%s\n", each[0].c_str());
   }
 }
 
 void part2(std::vector<std::vector<std::string>> crates,
            std::vector<std::vector<int>> directions) {
-  return;
+  std::printf("PART2\n");
+  for (auto each : directions) {
+    // std::printf("move %d @ %d to %d\n", each[0] - 1, each[1] - 1, each[2] -
+    // 1);
+    std::vector<std::string> temp;
+    for (int i = 0; i < each[0]; i++) {
+      auto crate = crates[each[1] - 1].front();
+      // std::printf("move %s\n", crate.c_str());
+      crates[each[1] - 1].erase(crates[each[1] - 1].begin());
+      temp.insert(temp.begin(), crate);
+    }
+    for(auto crate: temp) {
+      crates[each[2] - 1].insert(crates[each[2] - 1].begin(), crate);
+    }
+  }
+  for (auto each : crates) {
+    std::printf("%s\n", each[0].c_str());
+  }
 }
 } // namespace day5

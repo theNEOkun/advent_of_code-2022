9db38f2c909c57beb27775c9ed0fc34d7c009fa7
theNEOkun <sven@raswill.se>
2022-12-06 15:01:50 +0100

    Done with day6

--- src/day6/day6.cpp
+++ src/day6/day6.cpp
@@ -1,37 +1,43 @@
 #include "day6.hpp"
 
-namespace day6{
+namespace day6 {
 
 void part1(std::string arr);
 void part2(std::string arr);
-  
+
 void run() {
   auto file = readFile("../resources/input_day6");
   std::string str = file[0];
   part1(str);
+  part2(str);
+}
+
+int getInfo(std::string arr, int length) {
+  int counter = 0;
+  for (int i = 0; i < arr.length(); i++) {
+    counter++;
+    std::set<char> temp;
+    for(int n = 0; n < length; n++)
+      temp.insert(arr[i + n]);
+    if (temp.size() == length) {
+      counter += length - 1;
+      for (auto each : temp)
+        std::printf("%c\n", each);
+      return counter;
+    }
+  }
+  return -1;
 }
 
 void part1(std::string arr) {
-  int counter = 0;
-  std::vector<char> temp;
-  for(int i = 0; i < arr.length(); i++) {
-    counter++;
-    std::set<char> temp;
-    temp.insert(arr[i]);
-    temp.insert(arr[i + 1]);
-    temp.insert(arr[i + 2]);
-    temp.insert(arr[i + 3]);
-    if(temp.size() == 4) {
-      counter += 3;
-      for(auto each: temp)
-        std::printf("%c\n", each);
-      break;
-    }
-  }
-  std::printf("%d\n", counter);
+  auto resp = getInfo(arr, 4);
+  if(resp == -1) throw;
+  std::printf("Part1: %d\n", resp);
 }
 
 void part2(std::string arr) {
-
+  auto resp = getInfo(arr, 14);
+  if(resp == -1) throw;
+  std::printf("Part 2: %d\n", resp);
 }
-}
+} // namespace day6

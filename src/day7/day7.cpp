14cd7c9aa8c7e082112b4b9e5e114a1853d8768b
theNEOkun <sven@raswill.se>
2022-12-07 10:07:05 +0100

    Some formatting

--- src/day7/day7.cpp
+++ src/day7/day7.cpp
@@ -14,19 +14,18 @@ class info {
   info_type what_command;
 
 public:
-  info(std::string incoming, std::string incoming_2) : command(incoming), command_part2(incoming_2), what_command(info_type::COMMAND) {
-  }
-  info(long size, std::string fileName) : size(size), fileName(fileName), what_command(info_type::FILE) {}
-  info(std::string directory) : fileName(directory), what_command(info_type::DIRECTORY) {}
+  info(std::string incoming, std::string incoming_2)
+      : command(incoming), command_part2(incoming_2),
+        what_command(info_type::COMMAND) {}
+  info(long size, std::string fileName)
+      : size(size), fileName(fileName), what_command(info_type::FILE) {}
+  info(std::string directory)
+      : fileName(directory), what_command(info_type::DIRECTORY) {}
   info() : what_command(info_type::FAIL) {}
 
-  info_type getCommand() {
-    return this->what_command;
-  }
+  const info_type getCommand() { return this->what_command; }
 
-  long getFileSize() {
-    return this->size;
-  }
+  const long getFileSize() { return this->size; }
 };
 
 void part1(std::vector<std::string> &file);
@@ -40,7 +39,7 @@ void run() {
 info parseSingleCommand(std::string line) {
   auto listOfWords = getListOfWords(line, " ");
   if (listOfWords[0] == "$") {
-    if(listOfWords.size() > 2)
+    if (listOfWords.size() > 2)
       return info(listOfWords[1], listOfWords[2]);
     else
       return info(listOfWords[1], "");
@@ -61,17 +60,17 @@ void part1(std::vector<std::string> &file) {
   std::map<std::string, long> map;
   std::string current;
   long tally = 0;
-  for(auto each: file) {
+  for (auto each : file) {
     info command = parseSingleCommand(each);
-    switch(command.getCommand()) {
-      case info_type::DIRECTORY:
-        break;
-      case info_type::FILE:
-        break;
-      case info_type::COMMAND:
-        break;
-      default:
-        break;
+    switch (command.getCommand()) {
+    case info_type::DIRECTORY:
+      break;
+    case info_type::FILE:
+      break;
+    case info_type::COMMAND:
+      break;
+    default:
+      break;
     }
   }
 }

443158873c9d5adf0a4523cc52967afe7fb2d0ff
theNEOkun <sven@raswill.se>
2022-12-10 17:09:34 +0100

    Prepared for day 10

---
+++ src/day10/day10.cpp
@@ -0,0 +1,34 @@
+#include "day10.hpp"
+namespace day10 {
+
+void part1();
+void part2();
+
+void run(utils utils) {
+  std::cout << "DAY 1" << std::endl;
+  std::vector<std::string> file = utils.readFile("resources/input_day10");
+  std::vector<int> gnomes{0};
+  int index = 0;
+
+  for (auto each : file) {
+    if (each.size() == 0) {
+      gnomes.push_back(0);
+      index++;
+    } else {
+      gnomes[index] += std::stoi(each);
+    }
+  }
+
+  std::sort(gnomes.begin(), gnomes.end(), [](int &a, int &b) { return a > b; });
+
+  int largest = gnomes[0];
+
+  std::cout << "Largest:" << largest << std::endl;
+
+  std::cout << "Three largest: " << gnomes[0] + gnomes[1] + gnomes[2]
+            << std::endl;
+}
+
+void part1() {}
+void part2() {}
+} // namespace day10
---
+++ src/day10/day10.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "../utils/utils.hpp"
+
+namespace day1 {
+  void run(utils utils);
+}

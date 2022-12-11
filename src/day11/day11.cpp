e7164f1c385ec7e4d1d9905c0b9c5fd48d2b4266
theNEOkun <sven@raswill.se>
2022-12-11 22:32:03 +0100

    Done with day 11

--- src/day11/day11.cpp
+++ src/day11/day11.cpp
@@ -22,27 +22,30 @@
 
 namespace day11 {
 
-std::function<int(int, int)>
+std::function<uint64_t(uint64_t, uint64_t)>
 makeOperation(std::vector<std::string> operations) {
   if (operations[2] == "old") {
     if (operations[1] == "*") {
-      return [](int old1, int old2) -> int { return old1 * old2; };
+      return
+          [](uint64_t old1, uint64_t old2) -> uint64_t { return old1 * old2; };
     }
     if (operations[1] == "+")
-      return [](int old1, int old2) -> int { return old1 + old2; };
+      return
+          [](uint64_t old1, uint64_t old2) -> uint64_t { return old1 + old2; };
   }
   if (operations[1] == "*")
-    return [=](int old1, int old2) -> int {
+    return [=](uint64_t old1, uint64_t old2) -> uint64_t {
       return old1 * std::stoi(operations[2]);
     };
   else
-    return [=](int old1, int old2) -> int {
+    return [=](uint64_t old1, uint64_t old2) -> uint64_t {
       return old1 + std::stoi(operations[2]);
     };
 }
 
-std::function<int(int)> makeTest(int test, int trueMonkey, int falseMonkey) {
-  return [=](int testnum) -> int {
+std::function<uint64_t(uint64_t)> makeTest(uint64_t test, uint64_t trueMonkey,
+                                           uint64_t falseMonkey) {
+  return [=](uint64_t testnum) -> uint64_t {
     return (testnum % test == 0) ? trueMonkey : falseMonkey;
   };
 }
@@ -50,9 +53,11 @@ std::function<int(int)> makeTest(int test, int trueMonkey, int falseMonkey) {
 class Monkey {
 private:
   int name;
-  std::deque<int> items;
-  std::function<int(int, int)> operation;
-  std::function<int(int)> test;
+  std::deque<uint64_t> items;
+  std::function<uint64_t(uint64_t, uint64_t)> operation;
+  int test;
+  int trueMonkey;
+  int falseMonkey;
   int inspections = 0;
 
 public:
@@ -71,33 +76,35 @@ public:
         utils::splitString(utils::splitString(operationRow[1], " = ")[1]);
     operation = makeOperation(operations);
 
-    auto testNum = utils::splitString(monkeyInfo[3]).back();
-    auto trueMonkey = utils::splitString(monkeyInfo[4]).back();
-    auto falseMonkey = utils::splitString(monkeyInfo[5]).back();
-    test = makeTest(std::stoi(testNum), std::stoi(trueMonkey),
-                    std::stoi(falseMonkey));
+    test = std::stoi(utils::splitString(monkeyInfo[3]).back());
+    trueMonkey = std::stoi(utils::splitString(monkeyInfo[4]).back());
+    falseMonkey = std::stoi(utils::splitString(monkeyInfo[5]).back());
   }
 
   const int getName() const { return name; }
 
-  void recieveItem(int item) { items.push_back(item); }
+  const int getTest() const { return test; }
 
-  const int throwItem() {
-    int item = items.front();
-    items.pop_front();
-    int retVal = operation(item, item);
-    retVal /= 3;
+  void recieveItem(uint64_t item) { items.push_back(item); }
+
+  const uint64_t throwItem(bool managed) {
     inspections++;
+    uint64_t item = items.front();
+    items.pop_front();
+    uint64_t retVal = operation(item, item);
+    if (!managed) {
+      retVal /= 3;
+    }
     return retVal;
   }
 
-  int toMonkey(int item) { return test(item); }
+  uint64_t toMonkey(uint64_t item) { return (item % test == 0)? trueMonkey: falseMonkey; }
 
-  const std::deque<int> &getItems() const { return items; }
+  const std::deque<uint64_t> &getItems() const { return items; }
 
   bool hasItems() const { return !items.empty(); }
 
-  const int getInsp() const { return inspections; }
+  const uint64_t getInsp() const { return inspections; }
 
   void print() {
     std::cout << "Name: " << name << std::endl;
@@ -121,33 +128,50 @@ void run(utils utils) {
   std::vector<Monkey> monkeyList;
   for (auto i = 0; i < file.size(); i += 7) {
     std::vector<std::string> range;
-    for (int j = 0; j < 6; j++)
+    for (uint64_t j = 0; j < 6; j++)
       range.push_back(file[i + j]);
     Monkey temp{range};
     monkeyList.push_back(temp);
   }
   part1(monkeyList);
+  part2(monkeyList);
 }
 
 void part1(std::vector<Monkey> monkeyList) {
   for (int i = 0; i < 20; i++) {
-    std::cout << i << std::endl;
-    for (auto i = 0; i < monkeyList.size(); i++) {
-      while (monkeyList[i].hasItems()) {
-        int nItem = monkeyList[i].throwItem();
-        int toMonk = monkeyList[i].toMonkey(nItem);
+    for (auto &monkey : monkeyList) {
+      while (monkey.hasItems()) {
+        uint64_t nItem = monkey.throwItem(false);
+        uint64_t toMonk = monkey.toMonkey(nItem);
         monkeyList[toMonk].recieveItem(nItem);
       }
-    }
-    for (auto monkey : monkeyList) {
-      monkey.print();
     }
   }
 
   std::sort(monkeyList.begin(), monkeyList.end(),
             [](auto a, auto b) { return a > b; });
-  std::printf("part1: %d\n", monkeyList[0].getInsp() * monkeyList[1].getInsp());
+  std::printf("part1: %lu\n",
+              monkeyList[0].getInsp() * monkeyList[1].getInsp());
 }
 
-void part2(std::vector<Monkey> monkeyList) {}
+void part2(std::vector<Monkey> monkeyList) {
+  int testNum = 1;
+  for(auto each: monkeyList){
+    testNum *= each.getTest();
+  }
+  for (int i = 0; i < 10000; i++) {
+    for (auto &monkey : monkeyList) {
+      while (monkey.hasItems()) {
+        uint64_t nItem = monkey.throwItem(true) % testNum;
+        uint64_t toMonk = monkey.toMonkey(nItem);
+        monkeyList[toMonk].recieveItem(nItem);
+      }
+    }
+  }
+
+  std::sort(monkeyList.begin(), monkeyList.end(),
+            [](auto a, auto b) { return a > b; });
+  std::printf("part2: %lu\n",
+              monkeyList[0].getInsp() * monkeyList[1].getInsp());
+}
 } // namespace day11
